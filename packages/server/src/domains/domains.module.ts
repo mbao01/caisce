daf1f2a1e62eae7bfb091edc68b5a0f8714e1c00
Ayomide Bakare <bakkareayomideo@gmail.com>
2025-06-04 23:43:06 +0100

    feat(cache): setup redis with cache manager

--- packages/server/src/domains/domains.module.ts
+++ packages/server/src/domains/domains.module.ts
@@ -1,9 +1,24 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { CacheModule } from "@nestjs/cache-manager";
+import { createKeyv } from "@keyv/redis";
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot(), UsersModule],
+  imports: [
+    AuthModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        isGlobal: true,
+        ttl: configService.get("CACHE_TTL"),
+        stores: [createKeyv(configService.get("REDIS_URL"))],
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(),
+    UsersModule,
+  ],
 })
 export class DomainsModule {}
